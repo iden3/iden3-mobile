f944e96dced6008f13e27ef3d50a7812f013184e
Raúl Jareño <raul.jareno.diaz@gmail.com>
2020-04-03 16:51:31 +0200

    folders well organised

---
+++ android/iden3coreapi/src/test/java/com/iden3/iden3coreapi/ExampleUnitTest.kt
@@ -0,0 +1,26 @@
+package com.iden3.iden3coreapi
+
+import android.content.Context
+import org.junit.Test
+import org.junit.Assert.*
+import androidx.test.core.app.ApplicationProvider
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    val context = ApplicationProvider.getApplicationContext<Context>()
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun testIden3CoreAPI() {
+        val iden3CoreAPI = Iden3CoreAPI.instance
+        assertTrue(iden3CoreAPI != null)
+    }
+}
