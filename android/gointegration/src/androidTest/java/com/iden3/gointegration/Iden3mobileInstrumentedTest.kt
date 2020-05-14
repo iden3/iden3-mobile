c9bbea025f4f729dc353f73b49c9d0c7e3920cf1
Raúl Jareño <raul.jareno.diaz@gmail.com>
2020-05-14 12:15:20 +0200

    added more tests

--- android/gointegration/src/androidTest/java/com/iden3/gointegration/Iden3mobileInstrumentedTest.kt
+++ android/gointegration/src/androidTest/java/com/iden3/gointegration/Iden3mobileInstrumentedTest.kt
@@ -1,16 +1,21 @@
 package com.iden3.gointegration
 
 import android.content.Context
+import android.util.Log
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import iden3mobile.Iden3mobile
+import iden3mobile.*
+import junit.framework.TestCase.assertEquals
 import org.hamcrest.core.StringContains
+import org.junit.Assert
+import org.junit.Assert.assertNotEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.ExpectedException
 import org.junit.runner.RunWith
 import java.io.File
+import java.time.Instant
 
 
 /**
@@ -250,4 +255,256 @@ class Iden3mobileInstrumentedTest {
         ) { event -> print(event) }
     }
 
+    @Test
+    fun testRequestClaimSuccess() {
+        val file = File("$storePath/alias")
+        if (file.exists()) {
+            file.deleteRecursively()
+        }
+        file.mkdirs()
+        val identity = Iden3mobile.newIdentity(
+            "$storePath/alias",
+            "password",
+            web3Url,
+            1000,
+            null
+        ) { event -> print(event) }
+
+        val ticket = identity.requestClaim(issuerUrl, "${Instant.now()}")
+        Log.i("testRequestClaimSuccess","Ticket: $ticket")
+        Assert.assertNotEquals(null, ticket)
+        identity.stop()
+    }
+
+    @Test
+    fun testRequestClaimWithCallbackSuccess() {
+        val file = File("$storePath/alias")
+        if (file.exists()) {
+            file.deleteRecursively()
+        }
+        file.mkdirs()
+        val identity = Iden3mobile.newIdentity(
+            "$storePath/alias",
+            "password",
+            web3Url,
+            1000,
+            null
+        ) { event -> print(event) }
+
+        var ticketCounter = 0
+        identity.requestClaimWithCb(issuerUrl, "${Instant.now()}") { ticket, e ->
+            ticketCounter++
+            Log.i("testRequestClaimWithCallbackSuccess","Ticket: $ticket")
+            Assert.assertNotEquals(null, ticket)
+            Assert.assertEquals(null, e)
+            identity.stop()
+        }
+
+        // Wait for callback
+        while (ticketCounter < 1){
+            Log.i("testRequestClaimWithCallbackSuccess","Waiting for request claim ticket")
+            Thread.sleep(100)
+        }
+    }
+
+    @Test
+    fun testProveClaimSuccess() {
+        val file = File("$storePath/alias")
+        if (file.exists()) {
+            file.deleteRecursively()
+        }
+        file.mkdirs()
+        val identity = Iden3mobile.newIdentity(
+            "$storePath/alias",
+            "password",
+            web3Url,
+            1000,
+            null
+        ) { event -> print(event) }
+
+        var ticketCounter = 0
+        identity.requestClaimWithCb(issuerUrl, "${Instant.now()}") { ticket, e ->
+            Log.i("testProveClaimWithCallbackSuccess","Ticket: $ticket")
+            Assert.assertNotEquals(null, ticket)
+            Assert.assertEquals(null, e)
+            identity.proveClaimWithCb(verifierUrl, ticket.id) { success, exception ->
+                Log.i("testProveClaimWithCallbackSuccess", "Proving Clam success: $success exception: $exception")
+                ticketCounter++
+                Assert.assertEquals(true, success)
+                Assert.assertEquals(null, exception)
+                identity.stop()
+            }
+        }
+
+        // Wait for callback
+        while (ticketCounter < 1){
+            Log.i("testProveClaimWithCallbackSuccess","Waiting for request claim ticket")
+            Thread.sleep(100)
+        }
+
+
+        //identity.claimDB?.iterateClaimsJSON { key, claim ->
+        //    Log.i("testProveClaimWithCallbackSuccess", "Verify claim: $key.")
+
+        // true
+        //}
+    }
+
+    @Test
+    fun testProveClaimWithCallbackSuccess() {
+        var eventReceived = false
+        val file = File("$storePath/alias")
+        if (file.exists()) {
+            file.deleteRecursively()
+        }
+        file.mkdirs()
+        val identity = Iden3mobile.newIdentity(
+            "$storePath/alias",
+            "password",
+            web3Url,
+            1000,
+            null
+        ) { event ->
+            eventReceived = true
+            Log.i("fullFlow","Event Received: ${event.ticketId}.")
+            assertEquals(null, event.err)
+            print(event)
+        }
+
+        var isFinished = false
+        identity.requestClaimWithCb(issuerUrl, "${Instant.now()}") { ticket, e ->
+            Log.i("testProveClaimWithCallbackSuccess","Ticket: $ticket")
+            assertNotEquals(null, ticket)
+            assertEquals(null, e)
+            while (countClaims(identity) == 0 && !eventReceived) {
+                Log.i("testProveClaimWithCallbackSuccess","Waiting for claim to be available in the database")
+                Thread.sleep(1000)
+            }
+            var identityOnChain = false
+            identity.claimDB?.iterateClaimsJSON { key, claim ->
+                while (!identityOnChain) {
+                    identity.proveClaimWithCb(verifierUrl, key) { success, exception ->
+                        Log.i(
+                            "testProveClaimWithCallbackSuccess",
+                            "Proving Claim success: $success exception: $exception"
+                        )
+                        identityOnChain = !(exception != null && exception.message!!.contains("Identity not found on chain"))
+                        if (identityOnChain) {
+                            isFinished = true
+                            Assert.assertEquals(true, success)
+                            Assert.assertEquals(null, exception)
+                            identity.stop()
+                        }
+                    }
+                    Thread.sleep(2000)
+                }
+                true
+            }
+        }
+
+        // Wait for callback
+        while (!isFinished){
+            Log.i("testProveClaimWithCallbackSuccess","Waiting for request claim ticket")
+            Thread.sleep(1000)
+        }
+    }
+
+    @Test
+    fun testProveClaimWithCallbackErrorKeyNotFound() {
+        expectedException.expect(Exception::class.java)
+        expectedException.expectMessage(StringContains("key not found"))
+        val file = File("$storePath/alias")
+        if (file.exists()) {
+            file.deleteRecursively()
+        }
+        file.mkdirs()
+        val identity = Iden3mobile.newIdentity(
+            "$storePath/alias",
+            "password",
+            web3Url,
+            1000,
+            null
+        ) { event -> print(event) }
+
+        var exp : Exception? = null
+        var isFinished = false
+        identity.proveClaimWithCb(verifierUrl, "wrong_key") { success, exception ->
+            Log.i("testProveClaimWithCallbackErrorKeyNotFound", "Proving Clam success: $success exception: $exception")
+            assertEquals(false, success)
+            assertNotEquals(null, exception)
+            identity.stop()
+            exp = exception
+            isFinished = true
+        }
+
+        // Wait for callback
+        while (!isFinished){
+            Log.i("testProveClaimWithCallbackErrorKeyNotFound","Waiting for proving claim")
+            Thread.sleep(100)
+        }
+        throw exp!!
+    }
+
+    @Test
+    fun testProveClaimWithCallbackErrorIdentityNotFoundOnChain() {
+        expectedException.expect(Exception::class.java)
+        expectedException.expectMessage(StringContains("server: VerifyCredentialValidity(): Identity not found on chain or the queried block number is not yet on chain"))
+        var eventReceived = false
+        val file = File("$storePath/alias")
+        if (file.exists()) {
+            file.deleteRecursively()
+        }
+        file.mkdirs()
+        val identity = Iden3mobile.newIdentity(
+            "$storePath/alias",
+            "password",
+            web3Url,
+            1000,
+            null
+        ) { event ->
+            eventReceived = true
+            Log.i("fullFlow","Event Received: ${event.ticketId}.")
+            assertEquals(null, event.err)
+            print(event)
+        }
+        var exp : Exception? = null
+        var isFinished = false
+        identity.requestClaimWithCb(issuerUrl, "${Instant.now()}") { ticket, e ->
+            Log.i("testProveClaimWithCallbackSuccess","Ticket: $ticket")
+            assertNotEquals(null, ticket)
+            assertEquals(null, e)
+            while (countClaims(identity) == 0 && !eventReceived) {
+                Log.i("testProveClaimWithCallbackSuccess","Waiting for claim to be available in the database")
+                Thread.sleep(1000)
+            }
+            identity.claimDB?.iterateClaimsJSON { key, claim ->
+                identity.proveClaimWithCb(verifierUrl, key) { success, exception ->
+                    Log.i("testProveClaimWithCallbackSuccess", "Proving Claim success: $success exception: $exception")
+                    assertEquals(false, success)
+                    assertNotEquals(null, exception)
+                    identity.stop()
+                    exp = exception
+                    isFinished = true
+                }
+                true
+            }
+
+        }
+
+        // Wait for callback
+        while (!isFinished){
+            Log.i("testProveClaimWithCallbackSuccess","Waiting for request claim ticket")
+            Thread.sleep(1000)
+        }
+        throw exp!!
+    }
+
+    private fun countClaims(identity: Identity): Int {
+        var claimCounter = 0
+        identity.claimDB?.iterateClaimsJSON { key, claim ->
+            claimCounter++
+            true
+        }
+        return claimCounter
+    }
 }
