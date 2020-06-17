a7c990c03e21158c65fd00e236c6269877b9f324
Raúl Jareño <raul.jareno.diaz@gmail.com>
2020-06-17 16:07:49 +0200

    added ios example project

---
+++ ios/Iden3Example/iden3ExampleTests/Iden3ExampleTests.swift
@@ -0,0 +1,33 @@
+//
+//  Iden3ExampleTests.swift
+//  Iden3ExampleTests
+//
+//  Created by iden3 on 14/06/2020.
+//
+
+import XCTest
+@testable import Iden3Example
+
+class Iden3ExampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
